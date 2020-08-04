d3e3ddba7f3850ab03c11f89ee8bfb0740740b01
DESKTOP-MK8337K\12 <xiao865573228@gmail.com>
2020-08-05 00:13:16 +0800

    no message

--- pages/goods_list/goods_list.js
+++ pages/goods_list/goods_list.js
@@ -1,66 +1,31 @@
 // pages/goods_list/goods_list.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    goodsLists:[],
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  getGoodsLists(){
+    var _this = this;
+    var reqTask = wx.request({
+      url: 'https://api-hmugo-web.itheima.net/api/public/v1/goods/search',
+      data: {},
+      responseType: 'text',
+      success: (res) => {
+        console.log(res.data.message.goods);
+        _this.setData({
+          goodsLists:res.data.message.goods
+        })
+      }
+    });
+      
+  },
   onLoad: function (options) {
-
+    
+    this.getGoodsLists()
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
   onPullDownRefresh: function () {
 
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
   onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
 
   }
 })
